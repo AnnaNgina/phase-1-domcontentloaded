c7cf53684379b455de5a6e1dfedb0a5a41b25241
annastacia <annastaciangina96@gmail.com>
2022-06-27 21:13:10 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const common = require("mocha/lib/interfaces/common");
+
 // Your code goes here
+document.addEventListener('DOMContentLoaded', textEdit);
+function textEdit() {
+    let text = document.querySelector('#text');
+    text.innerHTML = 'This is really cool!';
+
+}
