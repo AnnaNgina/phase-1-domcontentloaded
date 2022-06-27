1182bf803bb56865704aeb43b660df9097daa862
annastacia <annastaciangina96@gmail.com>
2022-06-27 21:06:53 +0300

    commit tree 0264b2ae2c9a63b4761761b50eb538ec323a7978

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const common = require("mocha/lib/interfaces/common");
+
 // Your code goes here
+document.addEventListener('DOMContentLoaded', textEdit);
+function textEdit() {
+    let text = document.getElementById('text');
+    text.textContent = 'JavaScript is so cool. It lets me add text to my page programmatically';
+}
